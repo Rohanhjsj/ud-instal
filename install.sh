efafb5d7467ec8882d25c96aafd3d7019c7bd98f
Rohanhjsj <101053616+Rohanhjsj@users.noreply.github.com>
2022-09-30 20:38:01 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,1 @@
+apt-get install wget -y apt-get install wget -y apt-get install proot -y apt-get install git -y git clone https://github.com/MFDGaming/ubuntu-in-termux.git cd ubuntu-in-termux chmod +x ubuntu.sh ./ubuntu.sh -y ./startubuntu.sh
